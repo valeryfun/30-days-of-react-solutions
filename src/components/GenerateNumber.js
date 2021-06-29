ef434dc1d01bfaa3ae188980629d67b94c3a59f8
valeryfun <valery.fun.qi.en@gmail.com>
2021-06-29 21:56:18 +0800

    [ADD]generate numbers in random and map

--- src/components/GenerateNumber.js
+++ src/components/GenerateNumber.js
@@ -2,40 +2,16 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 
 const GenerateNumber = () => {
-	const numbers = [
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-	]
-
-	const oddNumber = numbers.filter(odd => odd % 2 !== 0)
-	const evenNumber = numbers.filter(odd => odd % 2 === 0)
+	const numbers = Array(100)
+		.fill()
+		.map((_, index) => index + 1)
+	numbers.sort(() => Math.random() - 0.5).slice(0, 8)
 
 	return (
 		<div className='day6-container'>
-			{oddNumber.map(oddNumbers => (
-				<Card className='day6-card' style={{ backgroundColor: 'orange' }}>
-					{oddNumbers}
-				</Card>
+			{numbers.map(randomNumber => (
+				<Card className='day6-card'>{randomNumber}</Card>
 			))}
-			{evenNumber.map(evenNumbers => (
-				<Card className='day6-card' style={{ backgroundColor: 'red' }}>
-					{evenNumbers}
-				</Card>
-			))}
-			{/* {numbers
-				.filter(even => even % 2 === 0)
-				.map(evenNumber => (
-					<Card className='day6-card' style={{ backgroundColor: 'red' }}>
-						{evenNumber}
-					</Card>
-				))
-				? numbers
-						.filter(odd => odd % 2 !== 0)
-						.map(oddNumber => (
-							<Card className='day6-card' style={{ backgroundColor: 'orange' }}>
-								{oddNumber}
-							</Card>
-						))
-				: null} */}
 		</div>
 	)
 }
