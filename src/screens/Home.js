454fce8f498f152ee2d80d0fa292b136b4c7443e
valeryfun <valery.fun.qi.en@gmail.com>
2021-07-10 15:42:10 +0800

    [ADD] Profile card

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Row } from 'react-bootstrap'
 import Day3FormContainer from '../components/Day3FormContainer'
 import TimeDate from '../components/TimeDate'
+import Day3Card from '../components/Day3Card'
 
 const Home = () => {
 	return (
@@ -8,6 +10,9 @@ const Home = () => {
 			<h1>
 				<TimeDate />
 			</h1>
+			<Row>
+				<Day3Card />
+			</Row>
 		</Day3FormContainer>
 	)
 }
